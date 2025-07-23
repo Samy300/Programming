51dbb29107f2bbf1e6d8a913536e7324e91b136f
Samy300 <146211724+Samy300@users.noreply.github.com>
2025-07-23 20:17:51 +0000

    Initial commit

---
+++ Samara.py
@@ -0,0 +1,23 @@
+import os
+
+# Function to define the ANSi Colors
+def text_color(color: str):
+    colors = {
+        "green" : "\033[0;32m",
+        "blue": "\033[0;34m",
+        "red" : "\033[0;31m",
+        "white": "\033[0;37m"         
+        }
+
+    def clr(func):
+        def wrapper(*args, **kwargs):
+            for line in func (*args, **kwargs):
+                yield f"{colors[color]}{line}\033[0m"
+            return wrapper
+    return color
+
+
+
+
+    
+
