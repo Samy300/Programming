7fcbe7a20530de6bf6e7136a7723e90eef1dada7
Jhanan727 <jhananvaca5@gmail.com>
2025-07-23 23:43:58 +0200

    Update Jhanan.py

--- Jhanan.py
+++ Jhanan.py
@@ -1,26 +1,24 @@
-import os
-
 class MainReader:
     def __init__(self, file_name):
         self.file_name = file_name
 
     def line_reader(self):
         if not os.path.exists(self.file_name):
-            yield "Unaviable"
+            yield "Unavailable"
             return
         with open(self.file_name) as file:
             for line in file:
                 yield line.strip()
-        
+
     def play(self):
         for line in self.line_generator():
             print(line)
-    
-    @text_color("white"):
+
+    @text_color("white")
     def line_generator(self):
         return self.line_reader()
-    
-    def _concat(self, other,color_class):
+
+    def _concat(self, other, color_class):
         output_file = "concatenated.txt"
         with open(output_file, "w") as out:
             for fname in (self.file_name, other.file_name):
@@ -28,9 +26,9 @@ class MainReader:
                     with open(fname) as f:
                         out.writelines(f)
         return color_class(output_file)
-    
+
     def __add__(self, other):
-        return self.concat(other, color_class = self.__class__)
+        return self._concat(other, color_class=self.__class__)
 
 
     
