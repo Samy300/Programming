7721433bbc8b18038594d3315b5df0bbb5c4ddc8
Samy300 <146211724+Samy300@users.noreply.github.com>
2025-07-24 17:10:09 +0200

    Update test_project.py

--- test_project.py
+++ test_project.py
@@ -1,6 +1,6 @@
 import os 
 import pytest
-from project import FileReader, GreenFileReader
+from project import FileReader, RedFileReader
 
 def test_line_reader_nonexisten():
     reader = FileReader("nonexistent.text")
@@ -14,4 +14,4 @@ def test_line_generator_color_white(tmp_path):
     reader = FileReader(str(file_path))
     lines = list(reader.line_generator())
 
-    assert "\033[0:37m]" in lines[0] and "\033[0m]" in lines[0]
+    assert "\031[0:37m]" in lines[0] and "\033[0m]" in lines[0]
